0b71ea590a8ed209c1785c694b8d8ef00f31037e
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-10-25 06:22:44 -0600

    added routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+lines (9 sloc)  301 Bytes
+  
+var router =require("express").Router();
+var path =require("path");
+
+
+router.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/notes.html"))
+})
+router.get("*", function(req, res){
+    res.sendFile(path.join(__dirname,"../public/index.html"))
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,15 @@
+
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoute");
+const apiRoutes = require("./routes/apiRoute");
+
+const app = express();
+const PORT = process.env.PORT || 8081;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
+
+app.listen(PORT,()=> console.log(`Listening on PORT : ${PORT}`));
