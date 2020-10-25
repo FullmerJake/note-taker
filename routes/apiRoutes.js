6aceb33e37207df6ac2dfabd50c8cfc918c991c8
Jake Fullmer <w.jake.fullmer@gmail.com>
2020-10-25 06:55:52 -0600

    added comments

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,19 @@
 var router =require("express").Router();
 var notes = require("../db/notes.js");
 
-
+// GET route 
 router.get("/notes", function(req, res){
     notes.getNotes()
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 })
+// POST route, adds a new note to the left side
 router.post("/notes", function(req, res){
     notes.addNotes(req.body)
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 })
+// DELETE route, takes the JSON object ID and uses removeNote function to delete the note.
 router.delete("/notes/:id", function(req, res){
     notes.removeNote(req.params.id)
     .then(() => res.json({ok: true}))
